c64c0646e36656d6431de944a71e72ed8f6cb486
mojo <Mohammad.Izadi.2003@gmail.com>
2021-05-14 01:31:06 +0430

    Yomi ship handled

--- ap/src/main/java/model/Card/Monsters/ExploderDragon.java
+++ ap/src/main/java/model/Card/Monsters/ExploderDragon.java
@@ -1,6 +1,5 @@
 package model.Card.Monsters;
 
-import controller.DuelControllers.Actoins.Destroy;
 import controller.DuelControllers.GameData;
 import model.Card.Monster;
 import model.Enums.CardMod;
@@ -11,12 +10,12 @@ public class ExploderDragon extends Monster {
 
     @Override
     public boolean attackIsNormal(GameData gameData) {
-        Monster AttackingMonster = (Monster) gameData.getSelectedCard();
-        if (getCardMod().equals(CardMod.OFFENSIVE_OCCUPIED) && AttackingMonster.getAttack() < getAttack()) {
+        Monster attackingMonster = (Monster) gameData.getSelectedCard();
+        if (getCardMod().equals(CardMod.OFFENSIVE_OCCUPIED) && attackingMonster.getAttack() < getAttack()) {
             return true;
         }
-        new Destroy(gameData).run(AttackingMonster);
-        new Destroy(gameData).run(this);
+        attackingMonster.handleDestroy(gameData);
+        this.handleDestroy(gameData);
         Printer.print("you attacked exploder dragon and both cards were destroyed");
         return false;
     }
@@ -24,8 +23,8 @@ public class ExploderDragon extends Monster {
     @Override
     protected void attackOffensiveMonster(Monster defendingMonster, GameData gameData) {
         if (getAttack() < defendingMonster.getAttack()) {
-            new Destroy(gameData).run(defendingMonster);
-            new Destroy(gameData).run(this);
+            defendingMonster.handleDestroy(gameData);
+            this.handleDestroy(gameData);
             Printer.print("both cards were destroyed and no one received any damage");
             return;
         }
--- ap/src/main/java/model/Card/Monsters/YomiShip.java
+++ ap/src/main/java/model/Card/Monsters/YomiShip.java
@@ -1,8 +1,13 @@
 package model.Card.Monsters;
+import controller.DuelControllers.GameData;
 import model.Card.Monster;
+import view.Printer.Printer;
 
 public class YomiShip extends Monster {
-    public void handleDestroy() {
 
+    @Override
+    public void handleDestroy(GameData gameData) {
+        super.handleDestroy(gameData);
+        gameData.getSelectedCard().handleDestroy(gameData);
     }
 }
