4a5d54e9ccf75939ebbf4fb5247e125beb14e2bd
mojo <Mohammad.Izadi.2003@gmail.com>
2021-05-14 01:43:39 +0430

    TheCalculator handled

--- ap/src/main/java/model/Card/Monsters/TheCalculator.java
+++ ap/src/main/java/model/Card/Monsters/TheCalculator.java
@@ -1,11 +1,32 @@
 package model.Card.Monsters;
 
-import model.Card.EffectTypes.Attack;
+import controller.DuelControllers.GameData;
+import model.Board.MonsterCardZone;
 import model.Card.Monster;
+import model.Enums.CardMod;
 
-public class TheCalculator extends Monster implements Attack {
+public class TheCalculator extends Monster {
+
     @Override
-    public void handleAttack() {
+    public void handleAttack(GameData gameData, int enemyId) {
+        this.setAttack(getCurrentAttack(gameData));
+        super.handleAttack(gameData, enemyId);
+    }
 
+    @Override
+    public boolean attackIsNormal(GameData gameData) {
+        this.setAttack(getCurrentAttack(gameData));
+        return true;
+    }
+
+    private int getCurrentAttack(GameData gameData){
+        int attack = 0;
+        MonsterCardZone attackerZone = gameData.getCurrentGamer().getGameBoard().getMonsterCardZone();
+        for (int i = 0; i < 5; i++) {
+            if (attackerZone.getCardById(i) != null && !((Monster)attackerZone.getCardById(i)).getCardMod().equals(CardMod.DEFENSIVE_HIDDEN)) {
+                attack += ((Monster)attackerZone.getCardById(i)).getLevel() * 300;
+            }
+        }
+        return attack;
     }
 }
