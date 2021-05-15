7da6d8f05824969ce1b7f24bb4ae9bf6c00739c5
mojo <Mohammad.Izadi.2003@gmail.com>
2021-05-15 16:40:13 +0430

    TheTricky handled

--- ap/src/main/java/model/Card/Monsters/TheTricky.java
+++ ap/src/main/java/model/Card/Monsters/TheTricky.java
@@ -1,11 +1,65 @@
 package model.Card.Monsters;
 
+import controller.DuelControllers.Actoins.Destroy;
 import controller.DuelControllers.GameData;
+import model.Board.Hand;
+import model.Card.Card;
 import model.Card.Monster;
+import view.GetInput;
+import view.Printer.Printer;
 
 public class TheTricky extends Monster {
-    @Override
-    public void handleSummon(GameData gameData, int numberOfSacrifices) {
-        super.handleSummon(gameData, numberOfSacrifices);
+
+    public void useEffect(GameData gameData) {
+        if (canUseEffect(gameData)) {
+            String command;
+            Hand handWithoutThisCard = cardsInHandExceptThisCard(gameData);
+            showChooseFromHand(handWithoutThisCard);
+
+            while (true) {
+                command = GetInput.getString();
+                if (command.matches("\\d+")) {
+                    if (Integer.parseInt(command) <= handWithoutThisCard.getCardsInHand().size()) {
+                        discardAndSummon(gameData, handWithoutThisCard.getCardsInHand().get(Integer.parseInt(command)));
+                        break;
+                    } else {
+                        Printer.print("invalid id");
+                        showChooseFromHand(handWithoutThisCard);
+                    }
+                } else if (command.matches("cancel")) {
+                    break;
+                } else {
+                    Printer.printInvalidCommand();
+                }
+
+            }
+        }
     }
+
+    private Hand cardsInHandExceptThisCard(GameData gameData) {
+        Hand handWithoutThisCard = (Hand) gameData.getCurrentGamer().getGameBoard().getHand().getCardsInHand().clone();
+        handWithoutThisCard.removeCard(handWithoutThisCard.getId(this));
+        return handWithoutThisCard;
+    }
+
+    private void discardAndSummon(GameData gameData, Card toDiscard) {
+        new Destroy(gameData).run(toDiscard);
+        gameData.moveCardFromOneZoneToAnother(this,
+                gameData.getCurrentGamer().getGameBoard().getHand(),
+                gameData.getCurrentGamer().getGameBoard().getMonsterCardZone());
+    }
+
+    private void showChooseFromHand(Hand hand) {
+        Printer.print("select a card id to discard:");
+        hand.showHand();
+    }
+
+    private boolean canUseEffect(GameData gameData) {
+        if (gameData.getCurrentGamer().getGameBoard().getHand().getCardsInHand().size() < 2) {
+            Printer.print("you do not have enough cards in your hand to special summon this card");
+            return false;
+        }
+        return true;
+    }
+
 }
