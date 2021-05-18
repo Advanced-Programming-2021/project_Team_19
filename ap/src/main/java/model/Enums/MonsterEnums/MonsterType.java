09cd4cea3bda865537d721ed32629b4b0687d2d2
mojo <Mohammad.Izadi.2003@gmail.com>
2021-05-18 17:54:26 +0430

    Enum serialized names added

--- ap/src/main/java/model/Enums/MonsterEnums/MonsterType.java
+++ ap/src/main/java/model/Enums/MonsterEnums/MonsterType.java
@@ -1,19 +1,36 @@
 package model.Enums.MonsterEnums;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum MonsterType {
-    BEAST_WARRIOR,
-    WARRIOR,
-    AQUA,
-    FIEND,
-    BEAST,
-    PYRO,
-    SPELLCASTER,
-    THUNDER,
-    DRAGON,
-    MACHINE,
-    ROCK,
-    INSECT,
-    CYBERSE,
+    @SerializedName("Fairy")
     FAIRY,
-    SEA_SERPENT
-}
+    @SerializedName("Insect")
+    INSECT,
+    @SerializedName("Rock")
+    ROCK,
+    @SerializedName("Sea Serpent")
+    SEA_SERPENT,
+    @SerializedName("Machine")
+    MACHINE,
+    @SerializedName("Dragon")
+    DRAGON,
+    @SerializedName("Thunder")
+    THUNDER,
+    @SerializedName("Spellcaster")
+    SPELLCASTER,
+    @SerializedName("Beast-Warrior")
+    BEAST_WARRIOR,
+    @SerializedName("Warrior")
+    WARRIOR,
+    @SerializedName("Aqua")
+    AQUA,
+    @SerializedName("Fiend")
+    FIEND,
+    @SerializedName("Beast")
+    BEAST,
+    @SerializedName("Pyro")
+    PYRO,
+    @SerializedName("Cyberse")
+    CYBERSE,
+    }
