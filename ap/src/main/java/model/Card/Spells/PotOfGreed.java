4dc37c7f65da7535644ac0bc0e292bae2347af08
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-05-25 18:13:37 +0430

    هرگز دلم برای کم و بیش غم نداشت / اری نداشت غم که غم بیش و کم نداشت.

--- ap/src/main/java/model/Card/Spells/PotOfGreed.java
+++ ap/src/main/java/model/Card/Spells/PotOfGreed.java
@@ -1,6 +1,7 @@
 package model.Card.Spells;
 
 import controller.DuelControllers.GameData;
+import model.Card.Card;
 import model.Card.Spell;
 import model.Data.ActivationData;
 import model.Enums.SpellsAndTraps.SpellTypes;
@@ -9,8 +10,17 @@ import model.Enums.Type;
 
 public class PotOfGreed extends Spell {
     @Override
-    public ActivationData activate(GameData gameData) {
-        return null;
+    public ActivationData activate(GameData gameData){
+        if(gameData.getCurrentGamer().getGameBoard().getDeckZone().getSize() < 2){
+            return new ActivationData(this, "There are not enough cards in the deck");
+        }
+        else{
+            Card firstCard = gameData.getCurrentGamer().getGameBoard().getDeckZone().getCard(1);
+            Card secondCard = gameData.getCurrentGamer().getGameBoard().getDeckZone().getCard(2);
+            gameData.moveCardFromOneZoneToAnother(firstCard, gameData.getCurrentGamer().getGameBoard().getDeckZone(),
+                    gameData.getCurrentGamer().getGameBoard().getHand());
+            return new ActivationData(this, "Successfully added two cards from the deck!");
+        }
     }
 
     public PotOfGreed(String name, String description, int price, Type type, SpellTypes spellType, Status status){
