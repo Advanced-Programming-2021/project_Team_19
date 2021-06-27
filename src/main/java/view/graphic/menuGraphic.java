5e3540c7133c8f13aad1da5feef9104401b08a99
Taha1506 <tahaakbarialvanagh@gmail.com>
2021-06-27 13:51:41 +0430

    Deck fixed to new Graphics

--- src/main/java/view/graphic/menuGraphic.java
+++ src/main/java/view/graphic/menuGraphic.java
@@ -41,7 +41,7 @@ public class menuGraphic extends Application {
         Scene scene = new Scene(new Pane(), sceneX, sceneY);
         stage.setScene(scene);
         scene.getStylesheets().add("CSS/Css.css");
-        new Shop().run(UserDataBaseController.getUserByUsername("mohammad"));
+        new Deck().run(UserDataBaseController.getUserByUsername("mohammad"));
         //run function of your menu for test here
         stage.show();
     }
